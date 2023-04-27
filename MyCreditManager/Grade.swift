c37fecee3884fd3cb5e7aa221b6c74048730496d
Taeyoung Won <hexter1994@gmail.com>
2023-04-27 23:58:03 +0900

    [Add] 열거형 Grade 추가

---
+++ MyCreditManager/Grade.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+enum Grade: String, CaseIterable {
+    case aPlus = "A+"
+    case a = "A"
+    case bPlus = "B+"
+    case b = "B"
+    case cPlus = "C+"
+    case c = "C"
+    case dPlus = "D+"
+    case d = "D"
+    case f = "F"
+    
+}
