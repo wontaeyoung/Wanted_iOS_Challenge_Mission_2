7e2933220745e8b73c9a77c6af76c6c2d237c35a
Taeyoung Won <hexter1994@gmail.com>
2023-04-29 13:59:19 +0900

    [Feat] 프로그램 기능 명세에 따른 프로토콜 프로퍼티/메서드 추가

--- MyCreditManager/Manager/Credit/CreditManageable.swift
+++ MyCreditManager/Manager/Credit/CreditManageable.swift
@@ -2,4 +2,23 @@ import Foundation
 
 protocol CreditManageable {
     
+    var students: [Student] { get set }
+    
+    func addStudent(name: String) -> Result<String,CreditManageError>
+    
+    func removeStudent(name: String) -> Result<String,CreditManageError>
+    
+    func addGrade(
+        studentName: String,
+        subjectName: String,
+        alphabetGrade: String
+    ) -> Result<String,CreditManageError>
+    
+    func removeGrade(
+        studentName: String,
+        subjectName: String
+    ) -> Result<String,CreditManageError>
+    
+    func getGPA(studentName: String) -> Result<String,CreditManageError>
+    
 }
