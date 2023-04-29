02b54188ff7f87b6194c47f338d846cc255d591f
Taeyoung Won <hexter1994@gmail.com>
2023-04-29 14:09:42 +0900

    [Feat] LocalizedError 채택, 케이스별 errorDescription 구현

--- MyCreditManager/Manager/Program/ProgramManageError.swift
+++ MyCreditManager/Manager/Program/ProgramManageError.swift
@@ -4,3 +4,20 @@ enum ProgramManageError: Error {
     case chooseInvalidFunction
     case inputInvalidText
 }
+
+extension ProgramManageError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .chooseInvalidFunction:
+            return NSLocalizedString(
+                "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.",
+                comment: self.localizedDescription
+            )
+        case .inputInvalidText:
+            return NSLocalizedString(
+                "입력이 잘못되었습니다. 다시 확인해주세요",
+                comment: self.localizedDescription
+            )
+        }
+    }
+}
