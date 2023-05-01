75b28c7e8e9d17fc8992c34b3f58a4c7931334db
Taeyoung Won <hexter1994@gmail.com>
2023-05-01 22:16:44 +0900

    [Feat] CreditManager 싱글톤 구현

--- MyCreditManager/Manager/Credit/CreditManager.swift
+++ MyCreditManager/Manager/Credit/CreditManager.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 final class CreditManager {
+
     // MARK: -Property
+    static let shared: CreditManager = .init()
     var students: [Student] = []
+    
+    // MARK: -Initializer
+    private init() { }
+    
 }
