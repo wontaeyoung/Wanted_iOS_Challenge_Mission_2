06303189829b17582326b265d36951623165deb2
Taeyoung Won <hexter1994@gmail.com>
2023-05-02 20:26:49 +0900

    [Add] RegExpPattern 열거형 추가
    사용자의 입력이 기능 요청에 대해 유효한 입력인지 검사하기 위한 정규식 케이스

--- MyCreditManager/Manager/Program/ProgramManager.swift
+++ MyCreditManager/Manager/Program/ProgramManager.swift
@@ -2,4 +2,11 @@ import Foundation
 
 final class ProgramManager: ProgramManageable {
     
+    // MARK: -Enum
+    private enum RegExpPattern: String {
+        case english = "^[a-zA-Z]*$"
+        case addGrade = "^[a-zA-Z]+\\s[a-zA-Z]+\\s.{1,2}$"
+        case removeGrade = "^[a-zA-Z]+\\s[a-zA-Z]$"
+    }
+    
 }
