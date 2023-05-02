0f1e8d66a3cb0d82f65add5d8cb6bce9e3958f96
Taeyoung Won <hexter1994@gmail.com>
2023-05-02 20:41:35 +0900

    [Feat] printProgramStartDescription 메서드 추가
    프로그램 시작 시 유저에게 메뉴를 안내해주는 문구를 출력하는 함수

--- MyCreditManager/Manager/Program/ProgramManager.swift
+++ MyCreditManager/Manager/Program/ProgramManager.swift
@@ -70,5 +70,10 @@ extension ProgramManager {
 // MARK: -출력 관련 메서드 익스텐션
 extension ProgramManager {
     
+    private func printProgramStartDescription() {
+        print("원하는 기능을 입력해주세요")
+        print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
+    }
+    
 }
 
