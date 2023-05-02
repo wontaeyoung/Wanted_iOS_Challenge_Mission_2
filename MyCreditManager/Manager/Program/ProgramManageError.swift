a1c83fd102051f6d62c69a48f8a951bfe6f3873a
Taeyoung Won <hexter1994@gmail.com>
2023-05-02 20:12:08 +0900

    [Fix] errorDescription 반환 타입을 NSLocalizedString -> String으로 변경, 케이스명을 간단하게 변경

--- MyCreditManager/Manager/Program/ProgramManageError.swift
+++ MyCreditManager/Manager/Program/ProgramManageError.swift
@@ -1,23 +1,19 @@
 import Foundation
 
 enum ProgramManageError: Error {
-    case chooseInvalidFunction
-    case inputInvalidText
+    case invalidMenu
+    case invalidInput
 }
 
 extension ProgramManageError: LocalizedError {
     var errorDescription: String? {
         switch self {
-        case .chooseInvalidFunction:
-            return NSLocalizedString(
-                "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.",
-                comment: self.localizedDescription
-            )
-        case .inputInvalidText:
-            return NSLocalizedString(
-                "입력이 잘못되었습니다. 다시 확인해주세요",
-                comment: self.localizedDescription
-            )
+            
+        case .invalidMenu:
+            return "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요."
+            
+        case .invalidInput:
+            return "입력이 잘못되었습니다. 다시 확인해주세요."
         }
     }
 }
