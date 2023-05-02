adaab9fcb3255c0f484c6428dd3de538c436b04d
Taeyoung Won <hexter1994@gmail.com>
2023-05-02 20:10:13 +0900

    [Fix] errorDescription의 리턴 타입을 NSLocalizedString에서 String으로 변경
    테스트 단계에서 NSLocalizedString 리턴 시 Fatal Error 발생

--- MyCreditManager/Manager/Credit/CreditManageError.swift
+++ MyCreditManager/Manager/Credit/CreditManageError.swift
@@ -13,34 +13,18 @@ enum CreditManageError: Error {
 extension CreditManageError: LocalizedError {
     
     var errorDescription: String? {
-        
         switch self {
-            
         case let .unfindable(name):
-            return NSLocalizedString(
-                "\(name) 학생을 찾지 못했습니다.",
-                comment: self.localizedDescription
-            )
+            return "\(name) 학생을 찾지 못했습니다."
+            
         case let .duplicateStudent(name):
-            return NSLocalizedString(
-                "\(name)은 이미 존재하는 학생입니다. 추가하지 않습니다.",
-                comment: self.localizedDescription
-            )
+            return "\(name)은 이미 존재하는 학생입니다. 추가하지 않습니다."
             
         case let .invalidGrade(alphabetGrade):
-            return NSLocalizedString(
-                "\(alphabetGrade)은 존재하지 않는 성적입니다.",
-                comment: self.localizedDescription
-            )
+            return "\(alphabetGrade)은 존재하지 않는 성적입니다."
             
-        case let .invalidSubject(
-            studentName,
-            subjectName
-        ):
-            return NSLocalizedString(
-                "\(studentName) 학생은 \(subjectName) 과목을 수강하지 않습니다.",
-                comment: self.localizedDescription
-            )
+        case let .invalidSubject(studentName, subjectName):
+            return "\(studentName) 학생은 \(subjectName) 과목을 수강하지 않습니다."
             
         }
         
